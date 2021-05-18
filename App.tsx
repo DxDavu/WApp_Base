db501cc35f39846287133601e6d16e9b011bcd33
Newtact <newtact@Suryapetas-MacBook-Air.local>
2021-05-18 18:38:47 +0530

    added background colors for TABS and added Icons

--- App.tsx
+++ App.tsx
@@ -5,6 +5,9 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
+import { color } from 'react-native-reanimated';
+import { FontAwesome5,Entypo } from '@expo/vector-icons';
+
 
 
 const Stack = createStackNavigator();
@@ -12,8 +15,37 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={
+        {
+          headerStyle: {
+            backgroundColor: "#075e54",
+            shadowOpacity: 0,
+            elevation: 0
+            
+          },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }
+      }}> 
+        <Stack.Screen name="Home" component={HomeScreen} 
+        options={
+          {
+            title : "WhatsApp",
+            headerRight: () => {
+              return <View style={
+                {
+                  flexDirection: 'row', width:60, justifyContent:'space-between',
+                  marginRight: 10, 
+              }
+                }>
+                <FontAwesome5 name="search" size={24} color="white" />
+                <Entypo name="dots-three-vertical" size={24} color="white" />
+              </View>
+            }
+          }
+        }
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import CameraScreen from "./CameraScreen";
 import ChatsScreen from "./ChatsScreen";
+import { Entypo } from "@expo/vector-icons";
 
 const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = () => {
   return (
-    <Tab.Navigator initialRouteName="Chats">
-      <Tab.Screen name="Camera" component={CameraScreen} />
+    <Tab.Navigator
+      initialRouteName="Chats"
+      tabBarOptions={{
+        activeTintColor: "#fff",
+        style: {
+          backgroundColor: "#075e54",
+        },
+        indicatorStyle: {
+          backgroundColor: "white",
+        },
+        labelStyle: {
+          fontWeight: "bold",
+        },
+        showIcon: true,
+      }}
+    >
+      <Tab.Screen
+        name="Camera"
+        component={CameraScreen}
+        options={{
+          tabBarIcon: () => <Entypo name="camera" size={24} color="white" />,
+          tabBarLabel: () => null,
+        }}
+      />
       <Tab.Screen name="Chats" component={ChatsScreen} />
     </Tab.Navigator>
   );
